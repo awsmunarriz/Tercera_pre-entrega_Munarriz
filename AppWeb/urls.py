ada01503a45659a1e0a70002455c8048d9801b4c
Mariano Munarriz <munarriz.aws@gmail.com>
2024-06-04 21:23:32 -0300

    add bootstrap template

--- AppWeb/urls.py
+++ AppWeb/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from AppWeb.views import lista_productos
+from AppWeb.views import lista_productos, inicio, productos, categorias, clientes, ordenes
 
 
 urlpatterns = [
     path('lista-productos/', lista_productos),
+    path('', inicio),
+    path('productos/', productos),
+    path('categorias/', categorias),
+    path('clientes/', clientes),
+    path('ordenes/', ordenes),
 ]
